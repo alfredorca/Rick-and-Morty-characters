59748ccf7f5cbfd308b3206fd5526534c73df82a
alfredorca <alfredorca@gmail.com>
2022-04-21 23:20:44 -0400

    Arrange images

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView} from "react-native";
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const req = await fetch(
+      "https://rickandmortyapi.com/api/character/?page=1"
+    );
+    const res = await req.json();
+
+    // console.log(res);
+    await setData(res.results);
+  }
+
+  if (data !== null) {
+    console.log("HERE", data);
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Rick & Morty !!!</Text>
+        <ScrollView contentContainerStyle={{minHeight: 900, paddingVertical:24}}>
+          {data.map((item, index) => {
+            return <Card style={styles.text} key={index} data={item} />;
+          })}
+        </ScrollView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  } else {
+    return <Text>Loading...</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical:24
+  },
+  text: {
+    color: "black",
   },
 });
---
+++ components/Card.js
@@ -0,0 +1,65 @@
+import { View, Text, StyleSheet, Image, Platform } from "react-native";
+import React from "react";
+
+export default function Card({ data }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: data.image,
+        }}
+      />
+      </View>
+      <View style={styles.textContainer}>
+        <Text>{data.name}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: 56,
+    borderRadius: 8,
+    minWidth: "90%",
+    margin: 8,
+    padding: 8,
+    ...Platform.select({
+      ios: {
+        shadowColor: "teal",
+        shadowOpacity: 0.8,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: 5,
+          width: 0,
+        },
+      },
+      android: {
+        elevation: 8,
+      },
+    }),
+  },
+  text: {
+    color: "black",
+  },
+  logo: {
+    width: 66,
+    height: 58,
+    borderRadius:8
+  },
+  imageContainer: {
+    maxWidth: 70,
+    margin: 8,
+  },
+  textContainer: {
+    width: '50%'
+  }
+});
