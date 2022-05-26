cc5932cb1f35bd0a919b0f380679e7ab35e743fc
alfredorca <alfredorca@gmail.com>
2022-05-25 23:43:45 -0400

    Start customizing individual screen

--- screens/character.js
+++ screens/character.js
@@ -1,5 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 function Character({ route }) {
   //
@@ -17,14 +24,30 @@ function Character({ route }) {
     );
     const res = await req.json();
     console.log(res);
-    await setResult(res.results);
+    await setResult(res);
+    // await speak(res);
   }
   // https://rickandmortyapi.com/api/character/2
   //
   if (result !== null) {
+    console.log("hereee -->", result);
     return (
-      <View>
-        <Text>Character</Text>
+      <View style={styles.container}>
+        <ImageBackground
+          source={{ uri: result.image }}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <LinearGradient
+            // Background Linear Gradient
+            colors={["rgba(0,0,0,0.8)", "transparent"]}
+            style={styles.background}
+          >
+            <Text style={styles.text}>{result.name}</Text>
+            {/* <TouchableOpacity style={{height:64, width:64, backgroundColor:'salmon'}}>
+            </TouchableOpacity> */}
+          </LinearGradient>
+        </ImageBackground>
       </View>
     );
   } else {
@@ -38,4 +61,24 @@ function Character({ route }) {
 
 export default Character;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  background:{
+    flex:1
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "transparent",
+  },
+
+});
